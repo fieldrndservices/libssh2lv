8cc040a4f560b16d56567279923abc6f417216ee
Christopher Field <cfield2@gmail.com>
2018-03-27 13:03:38 -0400

    Add all error codes from libssh2 to status

--- src/labssh2-status.c
+++ src/labssh2-status.c
@@ -31,6 +31,8 @@
  *   Christopher R. Field <chris@fieldrndservices.com>
  */
 
+#include "libssh2.h"
+
 #include "labssh2.h"
 
 static const char* UNKNOWN_STATUS = "Unknown status";
@@ -60,12 +62,50 @@ labssh2_status_string(labssh2_status_t status) {
         case LABSSH2_STATUS_ERROR_FREE: return "Free Memory Error";
         case LABSSH2_STATUS_ERROR_NULL_VALUE: return "Null Value Error";
         case LABSSH2_STATUS_ERROR_SOCKET_NONE: return "Socket None Error";
+        case LABSSH2_STATUS_ERROR_BANNER_RECV: return "Banner Receive Error";
         case LABSSH2_STATUS_ERROR_BANNER_SEND: return "Banner Send Error";
-        case LABSSH2_STATUS_ERROR_KEX_FAILURE: return "Key Exchange Error";
+        case LABSSH2_STATUS_ERROR_INVALID_MAC: return "Invalid MAC Error";
+        case LABSSH2_STATUS_ERROR_KEX_FAILURE: return "Key Exchange Failure Error";
         case LABSSH2_STATUS_ERROR_SOCKET_SEND: return "Socket Send Error";
+        case LABSSH2_STATUS_ERROR_KEY_EXCHANGE_FAILURE: return "Key Exchange Failure Error";
+        case LABSSH2_STATUS_ERROR_TIMEOUT: return "Timeout Error";
+        case LABSSH2_STATUS_ERROR_HOST_KEY_INITIALIZE: return "Host Key Iinitialize Error";
+        case LABSSH2_STATUS_ERROR_HOST_KEY_SIGNATURE: return "Host Key Signature Error";
+        case LABSSH2_STATUS_ERROR_DECRYPTION: return "Decryption Error";
         case LABSSH2_STATUS_ERROR_SOCKET_DISCONNECT: return "Socket Disconnect Error";
-        case LABSSH2_STATUS_ERROR_PROTOCAL: return "Protocol Error";
+        case LABSSH2_STATUS_ERROR_PROTOCOL: return "Protocol Error";
+        case LABSSH2_STATUS_ERROR_PASSWORD_EXPIRED: return "Password Expired Error";
+        case LABSSH2_STATUS_ERROR_FILE: return "File Error";
+        case LABSSH2_STATUS_ERROR_NONE: return "None Error";
+        case LABSSH2_STATUS_ERROR_AUTHENTICATION: return "Authentication Error";
+        case LABSSH2_STATUS_ERROR_PUBLIC_KEY_UNVERIFIED: return "Key Unverified Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_OUT_OF_ORDER: return "Channel Out of Order Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_FAILURE: return "Channel Failure Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_REQUEST_DENIED: return "Channel Request Denied Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_UNKNOWN: return "Channel Unknown Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_WINDOW_EXCEEDED: return "Channel Window Exceeded Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_PACKET_EXCEEDED: return "Channel Packet Exceeded Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_CLOSED: return "Channel Closed Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_EOF_SENT: return "Channel EOF Sent Error";
+        case LABSSH2_STATUS_ERROR_SCP_PROTOCOL: return "SCP Protocol Error";
+        case LABSSH2_STATUS_ERROR_ZLIB: return "zlib Error";
+        case LABSSH2_STATUS_ERROR_SOCKET_TIMEOUT: return "Socket Timeout Error";
+        case LABSSH2_STATUS_ERROR_SFTP_PROTOCOL: return "SFTP Protocol Error";
+        case LABSSH2_STATUS_ERROR_REQUEST_DENIED: return "Request Denied Error";
+        case LABSSH2_STATUS_ERROR_METHOD_NOT_SUPPORTED: return "Method Not Supported Error";
+        case LABSSH2_STATUS_ERROR_INVALID: return "Invalid Error";
+        case LABSSH2_STATUS_ERROR_INVALID_POLL_TYPE: return "Invalid Poll Type Error";
+        case LABSSH2_STATUS_ERROR_PUBLIC_KEY_PROTOCOL: return "Public Key Protocol Error";
         case LABSSH2_STATUS_ERROR_EXECUTE_AGAIN: return "Execute Again Error";
+        case LABSSH2_STATUS_ERROR_BUFFER_TOO_SMALL: return "Buffer Too Small Error";
+        case LABSSH2_STATUS_ERROR_BAD_USE: return "Bad Use Error";
+        case LABSSH2_STATUS_ERROR_COMMPRESS: return "Compress Error";
+        case LABSSH2_STATUS_ERROR_OUT_OF_BOUNDARY: return "Out of Boundary Error";
+        case LABSSH2_STATUS_ERROR_AGENT_PROTOCOL: return "Agent Protocol Error";
+        case LABSSH2_STATUS_ERROR_SOCKET_RECV: return "Socket Receive Error";
+        case LABSSH2_STATUS_ERROR_ENCRYPTION: return "Encryption Error";
+        case LABSSH2_STATUS_ERROR_BAD_SOCKET: return "Bad Socket Error";
+        case LABSSH2_STATUS_ERROR_KNOWN_HOSTS: return "Known Hosts Error"; 
         case LABSSH2_STATUS_ERROR_UNKNOWN_HASH_ALGORITHM: return "Unknown Hash Algorithm Error";
         case LABSSH2_STATUS_ERROR_HASH_UNAVAILABLE: return "Hash Unavailable Error";
         case LABSSH2_STATUS_ERROR_UNKNOWN_NAME_TYPE: return "Unknown Host Name Type Error";
@@ -83,25 +123,120 @@ labssh2_status_message(
         case LABSSH2_STATUS_MISMATCH: return "Host was found, but the keys did not match";
         case LABSSH2_STATUS_MATCH: return "Hosts and keys match";
         case LABSSH2_STATUS_NOT_FOUND: return "No host match was found";
-        case LABSSH2_STATUS_END_OF_HOSTS: return "There are no more hsots";
+        case LABSSH2_STATUS_END_OF_HOSTS: return "There are no more hosts";
         case LABSSH2_STATUS_OK: return "The function completed successfully";
         case LABSSH2_STATUS_ERROR_GENERIC: return "An unknown or generic error has occurred, good luck";
         case LABSSH2_STATUS_ERROR_MALLOC: return "Unable to allocate memory, most likely out of memory";
         case LABSSH2_STATUS_ERROR_FREE: return "Unable to deallocate, or free, memory";
         case LABSSH2_STATUS_ERROR_NULL_VALUE: return "The function argument/parameter cannot be NULL";
         case LABSSH2_STATUS_ERROR_SOCKET_NONE: return "The socket is invalid";
+        case LABSSH2_STATUS_ERROR_BANNER_RECV: return "Banner Receive Error";
         case LABSSH2_STATUS_ERROR_BANNER_SEND: return "Unable to send banner to remote host";
+        case LABSSH2_STATUS_ERROR_INVALID_MAC: return "Invalid MAC Error";
         case LABSSH2_STATUS_ERROR_KEX_FAILURE: return "Encryption key exchange with remote host failed";
         case LABSSH2_STATUS_ERROR_SOCKET_SEND: return "Unable to send data on socket";
+        case LABSSH2_STATUS_ERROR_KEY_EXCHANGE_FAILURE: return "Key Exchange Failure Error";
+        case LABSSH2_STATUS_ERROR_TIMEOUT: return "Timeout Error";
+        case LABSSH2_STATUS_ERROR_HOST_KEY_INITIALIZE: return "Host Key Iinitialize Error";
+        case LABSSH2_STATUS_ERROR_HOST_KEY_SIGNATURE: return "Host Key Signature Error";
+        case LABSSH2_STATUS_ERROR_DECRYPTION: return "Decryption Error";
         case LABSSH2_STATUS_ERROR_SOCKET_DISCONNECT: return "The socket was disconnected";
-        case LABSSH2_STATUS_ERROR_PROTOCAL: return "An invalid SSH protocol response was received on the socket";
+        case LABSSH2_STATUS_ERROR_PROTOCOL: return "An invalid SSH protocol response was received on the socket";
+        case LABSSH2_STATUS_ERROR_PASSWORD_EXPIRED: return "Password Expired Error";
+        case LABSSH2_STATUS_ERROR_FILE: return "File Error";
+        case LABSSH2_STATUS_ERROR_NONE: return "None Error";
+        case LABSSH2_STATUS_ERROR_AUTHENTICATION: return "Authentication Error";
+        case LABSSH2_STATUS_ERROR_PUBLIC_KEY_UNVERIFIED: return "Key Unverified Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_OUT_OF_ORDER: return "Channel Out of Order Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_FAILURE: return "Channel Failure Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_REQUEST_DENIED: return "Channel Request Denied Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_UNKNOWN: return "Channel Unknown Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_WINDOW_EXCEEDED: return "Channel Window Exceeded Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_PACKET_EXCEEDED: return "Channel Packet Exceeded Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_CLOSED: return "Channel Closed Error";
+        case LABSSH2_STATUS_ERROR_CHANNEL_EOF_SENT: return "Channel EOF Sent Error";
+        case LABSSH2_STATUS_ERROR_SCP_PROTOCOL: return "SCP Protocol Error";
+        case LABSSH2_STATUS_ERROR_ZLIB: return "zlib Error";
+        case LABSSH2_STATUS_ERROR_SOCKET_TIMEOUT: return "Socket Timeout Error";
+        case LABSSH2_STATUS_ERROR_SFTP_PROTOCOL: return "SFTP Protocol Error";
+        case LABSSH2_STATUS_ERROR_REQUEST_DENIED: return "Request Denied Error";
+        case LABSSH2_STATUS_ERROR_METHOD_NOT_SUPPORTED: return "Method Not Supported Error";
+        case LABSSH2_STATUS_ERROR_INVALID: return "Invalid Error";
+        case LABSSH2_STATUS_ERROR_INVALID_POLL_TYPE: return "Invalid Poll Type Error";
+        case LABSSH2_STATUS_ERROR_PUBLIC_KEY_PROTOCOL: return "Public Key Protocol Error";
         case LABSSH2_STATUS_ERROR_EXECUTE_AGAIN: return "Marked for non-blocking I/O but the call would block";
-        case LABSSH2_STATUS_ERROR_UNKNOWN_HASH_ALGORITHM: return "Only the MD5 and SHA1 algorithms are supported";
+        case LABSSH2_STATUS_ERROR_BUFFER_TOO_SMALL: return "Buffer Too Small Error";
+        case LABSSH2_STATUS_ERROR_BAD_USE: return "Bad Use Error";
+        case LABSSH2_STATUS_ERROR_COMMPRESS: return "Compress Error";
+        case LABSSH2_STATUS_ERROR_OUT_OF_BOUNDARY: return "Out of Boundary Error";
+        case LABSSH2_STATUS_ERROR_AGENT_PROTOCOL: return "Agent Protocol Error";
+        case LABSSH2_STATUS_ERROR_SOCKET_RECV: return "Socket Receive Error";
+        case LABSSH2_STATUS_ERROR_ENCRYPTION: return "Encryption Error";
+        case LABSSH2_STATUS_ERROR_BAD_SOCKET: return "Bad Socket Error";
+        case LABSSH2_STATUS_ERROR_KNOWN_HOSTS: return "Known Hosts Error"; 
+case LABSSH2_STATUS_ERROR_UNKNOWN_HASH_ALGORITHM: return "Only the MD5 and SHA1 algorithms are supported";
         case LABSSH2_STATUS_ERROR_HASH_UNAVAILABLE: return "The session has not been started, or the requested hash algorithm is not available";
         case LABSSH2_STATUS_ERROR_UNKNOWN_NAME_TYPE: return "Unknown host name type";
         case LABSSH2_STATUS_ERROR_UNKNOWN_KEY_ENCODING: return "Unknown key encoding";
         case LABSSH2_STATUS_ERROR_UNKNOWN_KEY_ALGORITHM: return "Unknown key algorithm";
         default: return UNKNOWN_STATUS;
+    }
+}
+
+labssh2_status_t 
+labssh2_status_from_result(int libssh2_result) 
+{
+    if (libssh2_result >= 0) {
+        return LABSSH2_STATUS_OK;
+    }
+    switch (libssh2_result) {
+        case LIBSSH2_ERROR_SOCKET_NONE: return LABSSH2_STATUS_ERROR_SOCKET_NONE;
+        case LIBSSH2_ERROR_BANNER_RECV: return LABSSH2_STATUS_ERROR_BANNER_RECV;
+        case LIBSSH2_ERROR_BANNER_SEND: return LABSSH2_STATUS_ERROR_BANNER_SEND;
+        case LIBSSH2_ERROR_INVALID_MAC: return LABSSH2_STATUS_ERROR_INVALID_MAC;
+        case LIBSSH2_ERROR_KEX_FAILURE: return LABSSH2_STATUS_ERROR_KEX_FAILURE;
+        case LIBSSH2_ERROR_ALLOC: return LABSSH2_STATUS_ERROR_MALLOC;
+        case LIBSSH2_ERROR_SOCKET_SEND: return LABSSH2_STATUS_ERROR_SOCKET_SEND;
+        case LIBSSH2_ERROR_KEY_EXCHANGE_FAILURE: return LABSSH2_STATUS_ERROR_KEY_EXCHANGE_FAILURE;
+        case LIBSSH2_ERROR_TIMEOUT: return LABSSH2_STATUS_ERROR_TIMEOUT;
+        case LIBSSH2_ERROR_HOSTKEY_INIT: return LABSSH2_STATUS_ERROR_HOST_KEY_INITIALIZE;
+        case LIBSSH2_ERROR_HOSTKEY_SIGN: return LABSSH2_STATUS_ERROR_HOST_KEY_SIGNATURE;
+        case LIBSSH2_ERROR_DECRYPT: return LABSSH2_STATUS_ERROR_DECRYPTION;
+        case LIBSSH2_ERROR_SOCKET_DISCONNECT: return LABSSH2_STATUS_ERROR_SOCKET_DISCONNECT;
+        case LIBSSH2_ERROR_PROTO: return LABSSH2_STATUS_ERROR_PROTOCOL;
+        case LIBSSH2_ERROR_PASSWORD_EXPIRED: return LABSSH2_STATUS_ERROR_PASSWORD_EXPIRED;
+        case LIBSSH2_ERROR_FILE: return LABSSH2_STATUS_ERROR_FILE;
+        case LIBSSH2_ERROR_METHOD_NONE: return LABSSH2_STATUS_ERROR_NONE;
+        case LIBSSH2_ERROR_AUTHENTICATION_FAILED: return LABSSH2_STATUS_ERROR_AUTHENTICATION;
+        case LIBSSH2_ERROR_PUBLICKEY_UNVERIFIED: return LABSSH2_STATUS_ERROR_PUBLIC_KEY_UNVERIFIED;
+        case LIBSSH2_ERROR_CHANNEL_OUTOFORDER: return LABSSH2_STATUS_ERROR_CHANNEL_OUT_OF_ORDER;
+        case LIBSSH2_ERROR_CHANNEL_FAILURE: return LABSSH2_STATUS_ERROR_CHANNEL_FAILURE;
+        case LIBSSH2_ERROR_CHANNEL_REQUEST_DENIED: return LABSSH2_STATUS_ERROR_CHANNEL_REQUEST_DENIED;
+        case LIBSSH2_ERROR_CHANNEL_UNKNOWN: return LABSSH2_STATUS_ERROR_CHANNEL_UNKNOWN;
+        case LIBSSH2_ERROR_CHANNEL_WINDOW_EXCEEDED: return LABSSH2_STATUS_ERROR_CHANNEL_WINDOW_EXCEEDED;
+        case LIBSSH2_ERROR_CHANNEL_PACKET_EXCEEDED: return LABSSH2_STATUS_ERROR_CHANNEL_PACKET_EXCEEDED;
+        case LIBSSH2_ERROR_CHANNEL_CLOSED: return LABSSH2_STATUS_ERROR_CHANNEL_CLOSED;
+        case LIBSSH2_ERROR_CHANNEL_EOF_SENT: return LABSSH2_STATUS_ERROR_CHANNEL_EOF_SENT;
+        case LIBSSH2_ERROR_SCP_PROTOCOL: return LABSSH2_STATUS_ERROR_SCP_PROTOCOL;
+        case LIBSSH2_ERROR_ZLIB: return LABSSH2_STATUS_ERROR_ZLIB;
+        case LIBSSH2_ERROR_SOCKET_TIMEOUT: return LABSSH2_STATUS_ERROR_SOCKET_TIMEOUT;
+        case LIBSSH2_ERROR_SFTP_PROTOCOL: return LABSSH2_STATUS_ERROR_SFTP_PROTOCOL;
+        case LIBSSH2_ERROR_REQUEST_DENIED: return LABSSH2_STATUS_ERROR_REQUEST_DENIED;
+        case LIBSSH2_ERROR_METHOD_NOT_SUPPORTED: return LABSSH2_STATUS_ERROR_METHOD_NOT_SUPPORTED;
+        case LIBSSH2_ERROR_INVAL: return LABSSH2_STATUS_ERROR_INVALID;
+        case LIBSSH2_ERROR_INVALID_POLL_TYPE: return LABSSH2_STATUS_ERROR_INVALID_POLL_TYPE;
+        case LIBSSH2_ERROR_PUBLICKEY_PROTOCOL: return LABSSH2_STATUS_ERROR_PUBLIC_KEY_PROTOCOL;
+        case LIBSSH2_ERROR_EAGAIN: return LABSSH2_STATUS_ERROR_EXECUTE_AGAIN;
+        case LIBSSH2_ERROR_BUFFER_TOO_SMALL: return LABSSH2_STATUS_ERROR_BUFFER_TOO_SMALL;
+        case LIBSSH2_ERROR_BAD_USE: return LABSSH2_STATUS_ERROR_BAD_USE;
+        case LIBSSH2_ERROR_COMPRESS: return LABSSH2_STATUS_ERROR_COMMPRESS;
+        case LIBSSH2_ERROR_OUT_OF_BOUNDARY: return LABSSH2_STATUS_ERROR_OUT_OF_BOUNDARY;
+        case LIBSSH2_ERROR_AGENT_PROTOCOL: return LABSSH2_STATUS_ERROR_AGENT_PROTOCOL;
+        case LIBSSH2_ERROR_SOCKET_RECV: return LABSSH2_STATUS_ERROR_SOCKET_RECV;
+        case LIBSSH2_ERROR_ENCRYPT: return LABSSH2_STATUS_ERROR_ENCRYPTION;
+        case LIBSSH2_ERROR_BAD_SOCKET: return LABSSH2_STATUS_ERROR_BAD_SOCKET;
+        case LIBSSH2_ERROR_KNOWN_HOSTS: return LABSSH2_STATUS_ERROR_KNOWN_HOSTS; 
+        default: return LABSSH2_STATUS_ERROR_GENERIC;
     }
 }
 
