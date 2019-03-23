f7cd223e5f2f26ce443d4a8c645531a91eb81b94
Christopher Field <cfield2@gmail.com>
2019-03-23 11:59:03 -0400

    Add SFTP attributes type definition and implementation
    This is needed for some of the SFTP functions.

---
+++ src/labssh2-sftp-attributes.c
@@ -0,0 +1,72 @@
+/*
+ * LabSSH2 - A LabVIEW-Friendly C library for libssh2
+ *
+ * Copyright (c) 2018 Field R&D Services, LLC. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * withoutmodification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the Field R&D Services nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Field R&D Services, LLC ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Field R&D Services, LLC BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Contributor(s):
+ *   Christopher R. Field <chris@fieldrndservices.com>
+ */
+
+#include <stdlib.h>
+
+#include "libssh2_sftp.h"
+
+#include "labssh2.h"
+#include "labssh2-status-private.h"
+#include "labssh2-sftp-attributes-private.h"
+
+labssh2_status_t
+labssh2_sftp_attributes_create(
+    labssh2_sftp_attributes_t** handle
+) {
+    *handle = NULL;
+    labssh2_sftp_attributes_t* attributes = malloc(sizeof(labssh2_sftp_attributes_t));
+    if (attributes == NULL) {
+        return LABSSH2_STATUS_ERROR_MALLOC;
+    }
+    LIBSSH2_SFTP_ATTRIBUTES* inner = malloc(sizeof(LIBSSH2_SFTP_ATTRIBUTES));
+    if (inner == NULL) {
+        free(attributes);
+        return LABSSH2_STATUS_ERROR_MALLOC;
+    }
+    attributes->inner = inner;
+    *handle = attributes;
+    return LABSSH2_STATUS_OK;
+}
+
+labssh2_status_t
+labssh2_sftp_attributes_destroy(
+    labssh2_sftp_attributes_t* handle
+) {
+    if (handle == NULL) {
+        return LABSSH2_STATUS_ERROR_NULL_VALUE;
+    }
+    free(handle->inner);
+    handle->inner = NULL;
+    free(handle);
+    return LABSSH2_STATUS_OK;
+}
