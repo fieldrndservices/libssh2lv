c58d2b65644493c88705ab55a46f30eca4fe0f6f
cfield <chris@fieldrndservices.com>
2019-04-11 12:27:00 -0400

    Bump to Visual Studio C++ Build Tools 2019

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 mkdir build32 & pushd build32
-cmake -G "Visual Studio 15 2017" ..
+cmake -G "Visual Studio 16 2019" ..
 cmake --build . --config Release > build.log
 popd
 mkdir build64 & pushd build64
-cmake -G "Visual Studio 15 2017 Win64" ..
+cmake -G "Visual Studio 16 2019 Win64" ..
 cmake --build . --config Release > build.log
 popd
