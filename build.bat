50c9dc1e474adb85f077d8c3bc96bb5d96392a42
cfield <chris@fieldrndservices.com>
2019-04-22 16:04:22 -0400

    Fix Windows build script to use Visual Studio 16 2019
    Visual Studio 16 2019 introduces multiple installations on the same machine. It also changes the platform selection method. Instead of "Win64" appended to the name of the generator for CMake, the "-A" flag with `x64` must be used.

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 mkdir build32 & pushd build32
-cmake -G "Visual Studio 16 2019" ..
+cmake -G "Visual Studio 16 2019" -A Win32 ..
 cmake --build . --config Release > build.log
 popd
 mkdir build64 & pushd build64
-cmake -G "Visual Studio 16 2019 Win64" ..
+cmake -G "Visual Studio 16 2019" -A x64 ..
 cmake --build . --config Release > build.log
 popd
