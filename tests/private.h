76552c4688cd5c9cf9780f308013b3d7a78bc343
Christopher Field <chris.field@theiascientific.com>
2021-11-16 12:51:46 -0500

    Fix project name in license header

--- tests/private.h
+++ tests/private.h
@@ -1,5 +1,5 @@
 /*
- * LabSSH2 - A LabVIEW-Friendly C library for libssh2
+ * LV-LIBSSH2 - A LabVIEW-Friendly C library for libssh2
  *
  * Copyright (c) 2018 Field R&D Services, LLC. All Rights Reserved.
  *
